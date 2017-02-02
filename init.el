0fe1d1ed34f18ffd3a12b9db2fc00aa1b3e404ea
Matthew <matthewhammond@outlook.com>
2017-02-02 21:40:23 +0000

    add evil mode

--- init.el
+++ init.el
@@ -169,7 +169,7 @@
  '(coffee-tab-width 2)
  '(package-selected-packages
    (quote
-    (yoshi-theme zweilight-theme kooten-theme rjsx-mode jsx-mode expand-region neotree nodejs-repl tagedit smex rainbow-delimiters projectile paredit magit ido-ubiquitous exec-path-from-shell clojure-mode-extra-font-locking cider))))
+    (evil yoshi-theme zweilight-theme kooten-theme rjsx-mode jsx-mode expand-region neotree nodejs-repl tagedit smex rainbow-delimiters projectile paredit magit ido-ubiquitous exec-path-from-shell clojure-mode-extra-font-locking cider))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
